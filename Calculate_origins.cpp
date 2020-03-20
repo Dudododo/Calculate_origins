45008443639274f11fac742f4cdf0d381d2fd030
Dudododo <a752601260@gmail.com>
2020-03-20 15:08:42 +0800

    添加项目文件。

---
+++ Calculate_origins.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cv::Mat scrMat = imread("IMG_1989.JPG", 0);
+	cv::Mat dstMat,outMat, statsMat, centrMat;
+	imshow("scrMat", scrMat);
+
+	threshold(scrMat, dstMat, 30, 255, THRESH_BINARY_INV);
+	
+	cv::Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(12,12));	
+
+	//腐蚀
+	morphologyEx(dstMat, dstMat, MORPH_ERODE, kernel);
+
+	int nComp = connectedComponentsWithStats(dstMat, outMat, statsMat, centrMat, 8, CV_32S);
+	cout << "the number of origins = " << nComp - 1 << endl;
+
+	imshow("dstMat", dstMat);
+
+	waitKey(0);
+	return 0;
+}
+
